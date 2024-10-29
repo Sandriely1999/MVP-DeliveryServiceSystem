ca2bc8fd9a40d7b878dc012cc3d70536ddb87bc0
Sandriely F M Toledo GTADSI33A <109091362+Sandriely1999@users.noreply.github.com>
2024-10-29 00:19:39 -0300

    Métodos de listar pedidos preparando e prontos e término de testar todos métodos

--- src/main/java/mvp_delivery_system/repositories/OrderItemRepository.java
+++ src/main/java/mvp_delivery_system/repositories/OrderItemRepository.java
@@ -2,6 +2,8 @@ package mvp_delivery_system.repositories;
 
 import mvp_delivery_system.entites.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
--- src/main/java/mvp_delivery_system/repositories/UserRepository.java
+++ src/main/java/mvp_delivery_system/repositories/UserRepository.java
@@ -2,7 +2,10 @@ package mvp_delivery_system.repositories;
 
 import mvp_delivery_system.entites.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
 
